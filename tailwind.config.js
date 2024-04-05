81f66ea473b743790d53c9e500bcbc798651cec0
Jake <jbmitchell747@gmail.com>
2024-04-05 15:04:23 -0500

    adds font awesome icons and restyles sidebar

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ module.exports = {
     "./src/app/pages/**/*.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-orange': '#f57e00',
+        'bg-black': '#191e1f',
+        'btn-blue': '#3a86ff',
+        'link-pink': '#ff69eb'
+      }
+    },
   },
   plugins: [],
 }
