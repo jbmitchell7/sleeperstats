6414664f59ae0835d37e563f72c3282fb6457269
Jake <jbmitchell747@gmail.com>
2024-04-08 09:39:17 -0500

    adds hamburger menu for mobile screens

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, inject } from '@angular/core';
+import { Component, ElementRef, Host, HostListener, OnDestroy, inject } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Subscription, tap } from 'rxjs';
@@ -19,14 +19,31 @@ import { CommonModule } from '@angular/common';
 export class SidebarComponent implements OnDestroy {
   readonly #store = inject(Store);
   readonly #router = inject(Router);
+  readonly #ref = inject(ElementRef);
   #sub!: Subscription;
   expanded = false;
+  mobile = false;
+  dropdownActive = false;
 
   constructor() {
+    this.#checkMobileScreen();
     this.#sub = this.#store
       .select(selectSharedData)
       .pipe(tap((state) => (this.expanded = state.sidebarExpanded)))
       .subscribe();
+  }
+
+  @HostListener('window:resize')
+  @HostListener('window:orientation')
+  recheck(): void {
+    this.#checkMobileScreen();
+  }
+
+  @HostListener('document:click', ['$event.target'])
+  checkClickLocation(target: HTMLElement): void {
+    if (this.mobile && !this.#ref.nativeElement.contains(target) && this.dropdownActive) {
+      this.dropdownActive = false;
+    }
   }
 
   ngOnDestroy(): void {
@@ -43,5 +60,15 @@ export class SidebarComponent implements OnDestroy {
 
   toggleExpanded(): void {
     this.#store.dispatch(toggleSidebarExpanded());
+  }
+
+  toggleDropdown(): void {
+    this.dropdownActive = !this.dropdownActive;
+  }
+
+  #checkMobileScreen(): void {
+    if (window.screen.width <= 500) {
+      this.mobile = true;
+    }
   }
 }
